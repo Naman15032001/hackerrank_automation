9021a682eecf22f2f71594e268d3dfb32e14424e
Naman Jain <namanjain15032001@gmail.com>
2022-10-17 18:56:57 +0530

    format

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
-const puppeteer=require('puppeteer');
+const puppeteer = require('puppeteer');
 
-const ans=require("./codes");
+const ans = require("./codes");
 
-const loginURL="https://www.hackerrank.com/auth/login";
+const loginURL = "https://www.hackerrank.com/auth/login";
 
-const uname="mount7788"
-const pwd="@naman2001N"
+const uname = "mount7788"
+const pwd = "@naman2001N"
 
-let browserOpen= puppeteer.launch({
-    headless:false,
+let browserOpen = puppeteer.launch({
+    headless: false,
 
-    args :['--start-maximized'],
+    args: ['--start-maximized'],
 
-    defaultViewport:null
+    defaultViewport: null
 });
 let page;
-browserOpen.then(function(browser){
+browserOpen.then(function (browser) {
 
 
-    let browserOpenPromise=browser.newPage();
+    let browserOpenPromise = browser.newPage();
     return browserOpenPromise;
 
 
 
-}).then(function(newTab){
+}).then(function (newTab) {
 
-    page=newTab;
+    page = newTab;
 
-    let hackerRank=page.goto(loginURL);
+    let hackerRank = page.goto(loginURL);
 
     return hackerRank;
 
 
 
-}).then(function(){
+}).then(function () {
 
-    let emailisEnter=page.type("input[type='text']",uname,{
-        delay:100
+    let emailisEnter = page.type("input[type='text']", uname, {
+        delay: 100
     });
-    return emailisEnter  
-}).then(function(){
+    return emailisEnter
+}).then(function () {
 
-    let pwdisEnter=page.type("input[type='password']",pwd,{
-        delay:100
+    let pwdisEnter = page.type("input[type='password']", pwd, {
+        delay: 100
     });
     return pwdisEnter
 
@@ -52,46 +52,50 @@ browserOpen.then(function(browser){
     //input[type="password"]button[type='button']
 
     //button[type="button"]
-}).then(function(){
+}).then(function () {
 
     //let loginEnter=page.click("button[type='button']");
-    let loginEnterp=page.click("button[data-analytics='LoginPassword']",{delay:50});
+    let loginEnterp = page.click("button[data-analytics='LoginPassword']", {
+        delay: 50
+    });
     return loginEnterp;
-}).then(function(){
+}).then(function () {
 
-    let algoPage=waitAndClick("div[data-automation='algorithms']",page);
-    
-   // page.click("div[data-automation='algorithms']",{delay:50});
+    let algoPage = waitAndClick("div[data-automation='algorithms']", page);
+
+    // page.click("div[data-automation='algorithms']",{delay:50});
     return algoPage;
 
-   
-}).then(function(){
 
-    let checkWarm=waitAndClick("input[value='warmup']",page);
+}).then(function () {
+
+    let checkWarm = waitAndClick("input[value='warmup']", page);
     return checkWarm;
-}).then(function(){
+}).then(function () {
 
-    let wait3Sec=page.waitFor(5000);
+    let wait3Sec = page.waitFor(5000);
     return wait3Sec;
-}).then(function(){
+}).then(function () {
 
 
     // document.querySelector $  $$ All
 
-    let allChallenge = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled",{delay:50});
+    let allChallenge = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled", {
+        delay: 50
+    });
     return allChallenge;
 
-}).then(function(questArr){
+}).then(function (questArr) {
 
     console.log(questArr.length);
 
-    let questionWillSolved=questionSolver(page,questArr[2],ans.answers[0]);
+    let questionWillSolved = questionSolver(page, questArr[2], ans.answers[0]);
 
     return questionWillSolved;
 
 
 
-}).catch(function(err){
+}).catch(function (err) {
     console.log(err);
 });
 
@@ -101,19 +105,19 @@ browserOpen.then(function(browser){
 
 
 
-function waitAndClick(selector,cpage){
+function waitAndClick(selector, cpage) {
 
-    return new Promise(function(resolve,reject){
+    return new Promise(function (resolve, reject) {
 
-        let waitForModule=cpage.waitForSelector(selector);
-        waitForModule.then(function(){
+        let waitForModule = cpage.waitForSelector(selector);
+        waitForModule.then(function () {
 
-            let clickModel=cpage.click(selector);
+            let clickModel = cpage.click(selector);
             return clickModel;
-        }).then(function(){
+        }).then(function () {
 
             resolve();
-        }).catch(function(err){
+        }).catch(function (err) {
 
             reject();
         })
@@ -126,72 +130,82 @@ function waitAndClick(selector,cpage){
 }
 
 
-function questionSolver(page,question,answer){
+function questionSolver(page, question, answer) {
 
 
-    return new Promise(function(resolve,reject){
+    return new Promise(function (resolve, reject) {
 
 
-        let clickQue=question.click();
-        clickQue.then(function(){
+        let clickQue = question.click();
+        clickQue.then(function () {
 
-            let textAr=waitAndClick(".monaco-editor.no-user-select.vs",page);
+            let textAr = waitAndClick(".monaco-editor.no-user-select.vs", page);
             return textAr
 
 
 
-        }).then(function(){
+        }).then(function () {
 
-            return waitAndClick(".checkbox-input",page);
-
-
-        }).then(function(){
-
-            return page.waitForSelector("textarea.custominput",page);
-        }).then(function(){
-
-            return page.type('textarea.custominput',answer,{delay:5})
+            return waitAndClick(".checkbox-input", page);
 
 
-        }).then(function(){
+        }).then(function () {
 
-            let dctrlPress=page.keyboard.down('Control');
+            return page.waitForSelector("textarea.custominput", page);
+        }).then(function () {
+
+            return page.type('textarea.custominput', answer, {
+                delay: 5
+            })
+
+
+        }).then(function () {
+
+            let dctrlPress = page.keyboard.down('Control');
             return dctrlPress;
-        }).then(function(){
+        }).then(function () {
 
-            let aPress=page.keyboard.press('A',{delay:100});
+            let aPress = page.keyboard.press('A', {
+                delay: 100
+            });
             return aPress;
-        }).then(function(){
+        }).then(function () {
 
-            let xPress=page.keyboard.press('X',{delay:100});
+            let xPress = page.keyboard.press('X', {
+                delay: 100
+            });
             return xPress;
-        }).then(function(){
+        }).then(function () {
 
-            let cunPress=page.keyboard.up('Control');
+            let cunPress = page.keyboard.up('Control');
             return cunPress;
-        }).then(function(){
+        }).then(function () {
 
-            let mtextAr=waitAndClick(".monaco-editor.no-user-select.vs",page);
+            let mtextAr = waitAndClick(".monaco-editor.no-user-select.vs", page);
             return mtextAr;
-        }).then(function(){
+        }).then(function () {
 
-            let ctrlPress=page.keyboard.down('Control');
+            let ctrlPress = page.keyboard.down('Control');
             return ctrlPress;
-        }).then(function(){
+        }).then(function () {
 
-            let aPress=page.keyboard.press('A',{delay:100});
+            let aPress = page.keyboard.press('A', {
+                delay: 100
+            });
             return aPress;
-        }).then(function(){
+        }).then(function () {
 
-            let vPress=page.keyboard.press('V',{delay:100});
+            let vPress = page.keyboard.press('V', {
+                delay: 100
+            });
             return vPress;
-        }).then(function(){
+        }).then(function () {
 
-            let dctrlPress=page.keyboard.up('Control');
+            let dctrlPress = page.keyboard.up('Control');
             return dctrlPress;
-        }).then(function(){
+        }).then(function () {
 
-            return waitAndClick(".ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled",page);
+            return waitAndClick(".ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled", page);
         })
     })
 
@@ -200,5 +214,5 @@ function questionSolver(page,question,answer){
 
 
 
-    
+
 }
